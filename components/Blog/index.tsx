07ea71fbd71c0f5f57c37b6a516796e8226f6241
Miexie R2 Network <sephjavier@gmail.com>
2024-10-15 10:08:40 +0800

    blogs

--- components/Blog/index.tsx
+++ components/Blog/index.tsx
@@ -34,6 +34,7 @@ const Blog = () => {
 
   return (
     <section className="py-20 lg:py-25 xl:py-30">
+      {blogPosts.length > 0 && (
       <div className="mx-auto max-w-c-1315 px-4 md:px-8 xl:px-0">
         {/* <!-- Section Title Start --> */}
         <div className="animate_top mx-auto text-center">
@@ -47,18 +48,21 @@ const Blog = () => {
         </div>
         {/* <!-- Section Title End --> */}
       </div>
+      )}
 
-      <div className="mx-auto mt-15 max-w-c-1280 px-4 md:px-8 xl:mt-15 xl:px-0">
-        {isLoading ? (
-          <LoadingState />
-        ) : (
-          <div className="grid grid-cols-1 gap-7.5 md:grid-cols-2 lg:grid-cols-3 xl:gap-10">
-            {blogPosts.map((blog, index) => (
-              <BlogPost blog={blog} key={index} />
-            ))}
-          </div>
-        )}
-      </div>
+      {blogPosts.length > 0 && (
+        <div className="mx-auto mt-15 max-w-c-1280 px-4 md:px-8 xl:mt-15 xl:px-0">
+          {isLoading ? (
+            <LoadingState />
+          ) : (
+            <div className="grid grid-cols-1 gap-7.5 md:grid-cols-2 lg:grid-cols-3 xl:gap-10">
+              {blogPosts.map((blog, index) => (
+                <BlogPost blog={blog} key={index} />
+              ))}
+            </div>
+          )}
+        </div>
+      )}
     </section>
   );
 };
