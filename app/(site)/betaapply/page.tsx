b5e218e8f45034698fc40708e71bc1b46d345fad
Chikaysci <kjabellar@gmail.com>
2024-10-18 08:22:38 +0800

    updated header

--- app/(site)/betaapply/page.tsx
+++ app/(site)/betaapply/page.tsx
@@ -4,16 +4,19 @@ import HubspotContactForm from "@/components/Betaform/form";
 import { getData, getDomain } from '@/lib/data';
 import Header from "@/components/Header";
 
+
 export const metadata: Metadata = {
   title: "Beta Signup to  ${domain}",
   description: "Beta signup options for ${domain}",
   // other metadata
 };
 
-export default async function JoinTheTeamPage  () {
+export default async function JoinTheTeamPage () {
   const c = await getData();
   const domain = getDomain();
   return (
+    <>
+    <Header c={c}/>
     <section className="pt-35 px-20 md:px-8 ml-20 mr-20">
     <div className="pb-20 px-20">
     <h1 className="light:text-primary capitalize dark:text-white border-txtgrad2 animate-once animate-jump mb-8 font-extrabold leading-none tracking-normal md:text-6xl md:tracking-tight">
@@ -25,6 +28,7 @@ export default async function JoinTheTeamPage  () {
       </div>
     </div>
     </section>
+    </>
   );
 };
 
